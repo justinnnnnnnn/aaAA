d5d01cb77fda420f6f46f3f02d734f4982e40a12
Justin Cheasty <cheasty@gmail.com>
2021-02-26 14:39:48 -0800

    Start cats rails

---
+++ w6d5/cats_99/app/models/cat.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Cat < ApplicationRecord
+  #add inclusion validation for allowed colors
+  #add sex options m/f
+  def age
+    
+    birthday = birth_date.to_s.to_i
+
+    2021 - birthday
+  end
+end
