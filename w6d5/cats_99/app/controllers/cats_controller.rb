fbfeb4457a406490ac750819f054aa931189d5d6
Justin Cheasty <cheasty@gmail.com>
2021-02-26 16:37:41 -0800

    create views for index and show

--- w6d5/cats_99/app/controllers/cats_controller.rb
+++ w6d5/cats_99/app/controllers/cats_controller.rb
@@ -1,7 +1,12 @@
 class CatsController < ApplicationController
-    def index
-        @cats= Cat.all
+  def index
+    @cats = Cat.all
 
-        render: index
-    end
+    render :index
+  end
+
+  def show
+    @cat = Cat.find_by(id: params[:id])
+    render :show
+  end
 end
