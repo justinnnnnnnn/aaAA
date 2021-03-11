553d9c6cb181dff949a3c36528dc898efe0086e3
Justin Cheasty <cheasty@gmail.com>
2021-03-10 17:21:30 -0800

    callback success

--- w8D3/phase_2_enumerable.js
+++ w8D3/phase_2_enumerable.js
@@ -1,11 +1,22 @@
 function callback(el) {
-  console.log(el + 1);
+  return el * 2
 };
 
-Array.prototype.myEach = function () {
+// Array.prototype.myEach = function () {
 
-  this.forEach(el => callback(el));
-  return this
-}
+//   this.forEach(el => callback(el));
+//   return this
+// }
 
-console.log([1, 2, 3,4].myEach());
+// console.log([1, 2, 3, 4].myEach());
+
+Array.prototype.myMap = function () {
+  let mapped = [];
+
+  for (let i = 0; i < this.length; i++) {
+    mapped.push(callback(this[i]));
+  };
+  return mapped;
+};
+
+console.log([1, 2, 3, 4].myMap());
