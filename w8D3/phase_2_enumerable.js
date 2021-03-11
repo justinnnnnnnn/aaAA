566af157e49ba495573eb7d1737a836db4575440
tpadgett <tim_padgett@yahoo.com>
2021-03-10 17:30:03 -0800

    final commit

--- w8D3/phase_2_enumerable.js
+++ w8D3/phase_2_enumerable.js
@@ -1,7 +1,3 @@
-function callback(el) {
-  return el * 2
-};
-
 // Array.prototype.myEach = function () {
 
 //   this.forEach(el => callback(el));
@@ -10,13 +6,29 @@ function callback(el) {
 
 // console.log([1, 2, 3, 4].myEach());
 
-Array.prototype.myMap = function () {
-  let mapped = [];
+// Array.prototype.myMap = function () {
+//   let mapped = [];
 
-  for (let i = 0; i < this.length; i++) {
-    mapped.push(callback(this[i]));
-  };
-  return mapped;
+//   for (let i = 0; i < this.length; i++) {
+//     mapped.push(callback(this[i]));
+//   };
+//   return mapped;
+// };
+
+function callback(el) {
+  return el * 2
 };
 
-console.log([1, 2, 3, 4].myMap());
+Array.prototype.myReduce = function () {
+  let accumulator = this[0];
+  if (this.length < 2 ) {
+    return accumulator;
+  };
+
+  for (let i = 1; i < this.length; i++) {
+    accumulator =  accumulator + callback(i);
+  };
+  return accumulator;
+};
+
+console.log([1, 2, 3, 4].myReduce());
