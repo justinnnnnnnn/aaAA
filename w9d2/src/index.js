c734682e7c161886d9e0a013d6bc8e765ce88392
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-03-16 09:20:57 -1000

    change to index.js

--- w9d2/src/index.js
+++ w9d2/src/index.js
@@ -1,1 +1,10 @@
-console.log("Webpack is working")
+const MovingObject = require("./moving_object.js");
+window.MovingObject = MovingObject;
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  const canvas = document.getElementById("game-canvas");
+  canvas.getContext("2d");
+  console.log("DOM fully loaded and parsed");
+});
+
+console.log("Webpack is working");
--- w9d2/src/moving_object.js
+++ w9d2/src/moving_object.js
@@ -1,10 +1,30 @@
-export default class MovingObject() {
-  constructor (pos, vel, radius, color) {
+function MovingObject() {
+  constructor(pos, vel, radius, color) {
+    this.pos = pos
     this.x = pos[0];
     this.y = pos[1];
+    this.vel = vel
     this.xVel = vel[0];
     this.yVel = vel[1];
     this.radius = radius;
     this.color = color;
   }
 }
+
+MovingObect.prototype.draw = function (ctx) {
+  ctx.fillStyle = this.color
+  ctx.beginPath()
+
+  ctx.arc(
+    this.x,
+    this.y,
+    this.radius,
+    0,
+    2 * Math.PI,
+    false
+  )
+  ctx.fill()
+}
+
+
+module.exports = MovingObject
