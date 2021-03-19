deef09812a916c174057a71edd2fc495ef7f48ca
Justin Cheasty <cheasty@gmail.com>
2021-03-19 14:17:15 -0700

    create warmup and some clock

--- pocket_projects/src/index.js
+++ pocket_projects/src/index.js
@@ -1,2 +1,5 @@
 import warmUp from "./warmup";
 
+
+import clock from "./clock";
+
--- pocket_projects/src/warmup.js
+++ pocket_projects/src/warmup.js
@@ -2,7 +2,14 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
+    const newP = document.createElement("p");
+  
+    const newContent = document.createTextNode(string);
+  
+    newP.appendChild(newContent);
 
+    htmlElement.appendChild(newP)
 };
 
 htmlGenerator('Party Time.', partyHeader);
+htmlGenerator("I <3 Vanilla DOM manipulation.", partyHeader);
