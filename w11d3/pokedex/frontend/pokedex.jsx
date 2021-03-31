6ed65943c49e7eeaf0d4cb786c2dbfc6b5be15d3
Justin Cheasty <cheasty@gmail.com>
2021-03-31 14:44:46 -0700

    create front end and working show page

---
+++ w11d3/pokedex/frontend/pokedex.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("It's working")
+  ReactDOM.render(<h1>Pokedex</h1>, document.getElementById('root'))
+})
